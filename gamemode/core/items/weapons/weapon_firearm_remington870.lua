4abee7bd876077a21e906fdfcf71c2b6356d2238
Mickee11 <107945985+Mickee11@users.noreply.github.com>
2022-09-06 15:34:39 -0500

    Comp-romise
    Compromise

--- gamemode/core/items/weapons/weapon_firearm_remington870.lua
+++ gamemode/core/items/weapons/weapon_firearm_remington870.lua
@@ -17,7 +17,7 @@ ITEM.SellPrice          = 12500
 
 ITEM.Variants			= {
 	Default = {
-		WeaponClass = "eternity_firearm_remington870_S_18",
+		WeaponClass = "eternity_firearm_remington870_S18",
 		Model = "models/weapons/smc/r870/w_remington_m870.mdl",
 		Skin = 0,
 		Bodygroups = {},
@@ -26,68 +26,6 @@ ITEM.Variants			= {
 		Refund = 2,
 		Description = "The base model of the Remington 870.",
 		Width = 6,
-		Height = 1
-	},
-	LongBarrel20 = {
-		WeaponClass = "eternity_firearm_remington870_S_20",
-		Model = "models/weapons/smc/r870/w_remington_m870.mdl",
-		Skin = 0,
-		Bodygroups = {[1] = 1},
-		Title = "Extended",
-		Cost = 0,
-		Refund = 2,
-		Description = "A modified version of the Remington 870.",
-		Width = 7,
-		Height = 1
-	},
-	ShortBarrel16 = {
-		WeaponClass = "eternity_firearm_remington870_S_16",
-		Model = "models/weapons/smc/r870/w_remington_m870.mdl",
-		Skin = 0,
-		Bodygroups = {[1] = 2},
-		Title = "Short Barrel",
-		Cost = 0,
-		Refund = 2,
-		Description = "A modified version of the Remington 870.",
-		Width = 4,
-		Height = 1
-	},
-	DefaultNoStock = {
-	WeaponClass = "eternity_firearm_remington870_NS_18",
-		Model = "models/weapons/smc/r870/w_remington_m870.mdl",
-		Skin = 0,
-		Bodygroups = {[2] = 1},
-		Title = "No Stock",
-		Cost = 0,
-		Refund = 2,
-		Description = "A modified version of the Remington 870.",
-		Width = 5,
-		Height = 1
-	},
-	LongBarrel20NoStock = {
-		WeaponClass = "eternity_firearm_remington870_NS_20",
-		Model = "models/weapons/smc/r870/w_remington_m870.mdl",
-		Skin = 0,
-		Bodygroups = {[1] = 1,
-			   		  [2] = 1},
-		Title = "Extended No Stock",
-		Cost = 0,
-		Refund = 2,
-		Description = "A modified version of the Remington 870.",
-		Width = 5,
-		Height = 1
-	},
-		ShortBarrel16NoStock = {
-		WeaponClass = "eternity_firearm_remington870_NS_16",
-		Model = "models/weapons/smc/r870/w_remington_m870.mdl",
-		Skin = 0,
-		Bodygroups = {[1] = 2,
-			          [2] = 1},
-		Title = "CQB",
-		Cost = 0,
-		Refund = 2,
-		Description = "A modified version of the Remington 870.",
-		Width = 3,
 		Height = 1
 	},
 }
